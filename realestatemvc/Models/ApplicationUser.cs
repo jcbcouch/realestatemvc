a4eef0d8a561ac457e66a3e45bbf152ced1ec607
jcbcouch <jcbcouch@gmail.com>
2025-04-06 14:57:15 -0500

    Add project files.

---
+++ realestatemvc/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace realestatemvc.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+        [NotMapped]
+        public string RoleId { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
