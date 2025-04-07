d68b910e1965faed53fdef6a8a35d38962e89ed7
jcbcouch <jcbcouch@gmail.com>
2025-04-06 19:31:12 -0500

    added generic repository with a listings service

---
+++ realestatemvc/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using realestatemvc.Data;
+
+namespace realestatemvc.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        Task Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+        Task SaveChanges();
+    }
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+
+        }
+
+        public async Task Add(T entity)
+        {
+            await dbSet.AddAsync(entity);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            IQueryable<T> all = dbSet;
+            return all;
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+
+        public async Task SaveChanges()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ realestatemvc/Services/ListingService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using realestatemvc.Data;
+using realestatemvc.Models;
+using realestatemvc.Repository;
+
+namespace realestatemvc.Services
+{
+    public interface IListingService
+    {
+        Task AddListing(Listing listing);
+    }
+
+    public class ListingService : IListingService
+    {
+        private ApplicationDbContext _db;
+        public IRepository<Listing> _listing { get; private set; }
+
+        public ListingService(ApplicationDbContext db)
+        {
+            _db = db;
+            _listing = new Repository<Listing>(_db);
+        }
+
+        public async Task AddListing(Listing listing)
+        {
+            await _listing.Add(listing);
+            await _listing.SaveChanges();
+        }
+    }
+}
